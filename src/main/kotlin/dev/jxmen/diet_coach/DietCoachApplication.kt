36578982b993195b04ecf028adaa85d57f4543f8
jxmen <sprnd645@gmail.com>
2025-06-18 23:40:49 +0900

    build: spring 프로젝트 추가
    https://start.spring.io/

---
+++ src/main/kotlin/dev/jxmen/diet_coach/DietCoachApplication.kt
@@ -0,0 +1,11 @@
+package dev.jxmen.diet_coach
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DietCoachApplication
+
+fun main(args: Array<String>) {
+	runApplication<DietCoachApplication>(*args)
+}
---
+++ src/test/kotlin/dev/jxmen/diet_coach/DietCoachApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.jxmen.diet_coach
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DietCoachApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
