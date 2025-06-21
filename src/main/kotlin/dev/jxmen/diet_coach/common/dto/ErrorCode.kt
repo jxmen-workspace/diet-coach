8ade26249f82179c900657ba11115ad2eb567c93
jxmen <sprnd645@gmail.com>
2025-06-21 22:22:39 +0900

    feat: 공통된 응답을 위한 클래스 파일 추가
    ApiResponse외 ErrorCode, ErrorType 등 코드 추가

---
+++ src/main/kotlin/dev/jxmen/diet_coach/common/dto/ErrorCode.kt
@@ -0,0 +1,11 @@
+package dev.jxmen.diet_coach.common.dto
+
+enum class ErrorCode {
+    // E: Error
+    E500,
+
+    // I: Image 관련 에러
+    I001,
+    I002,
+}
+
---
+++ src/main/kotlin/dev/jxmen/diet_coach/common/dto/ErrorType.kt
@@ -0,0 +1,12 @@
+package dev.jxmen.diet_coach.common.dto
+
+import org.springframework.boot.logging.LogLevel
+import org.springframework.http.HttpStatus
+
+enum class ErrorType(val status: HttpStatus, val code: ErrorCode, val message: String, val logLevel: LogLevel) {
+    DEFAULT_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, ErrorCode.E500, "An unexpected error has occurred.", LogLevel.ERROR),
+
+    // I: Image 관련 에러
+    REQUIRED_IMAGE_MISSING(HttpStatus.BAD_REQUEST, ErrorCode.I001, "이미지는 반드시 있어야 합니다.", LogLevel.WARN),
+    UNSUPPORTED_IMAGE_TYPE(HttpStatus.BAD_REQUEST, ErrorCode.I002, "허용되지 않은 이미지 형식입니다.", LogLevel.WARN),
+}
