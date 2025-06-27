ebb9c8ec1b27423178bf8d5f5936a39af94be059
jxmen <sprnd645@gmail.com>
2025-06-27 12:54:24 +0900

    chore: 지원하지 않는 이미지 타입 status code 415로 변경

--- src/main/kotlin/dev/jxmen/diet_coach/common/dto/ErrorType.kt
+++ src/main/kotlin/dev/jxmen/diet_coach/common/dto/ErrorType.kt
@@ -8,6 +8,6 @@ enum class ErrorType(val status: HttpStatus, val code: ErrorCode, val message: S
 
     // I: Image 관련 에러
     REQUIRED_IMAGE_MISSING(HttpStatus.BAD_REQUEST, ErrorCode.I001, "이미지는 반드시 있어야 합니다.", LogLevel.WARN),
-    UNSUPPORTED_IMAGE_TYPE(HttpStatus.BAD_REQUEST, ErrorCode.I002, "지원하지 않는 이미지 형식입니다.", LogLevel.WARN),
+    UNSUPPORTED_IMAGE_TYPE(HttpStatus.UNSUPPORTED_MEDIA_TYPE, ErrorCode.I002, "지원하지 않는 이미지 형식입니다.", LogLevel.WARN),
     UNPROCESSABLE_FOOD_IMAGE(HttpStatus.UNPROCESSABLE_ENTITY, ErrorCode.I003, "처리할 수 없는 음식 이미지입니다.", LogLevel.WARN),
 }
